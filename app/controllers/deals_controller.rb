713349175130aaad294eeec6c0d68940bee0fb77
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-02-25 21:52:23 +0900

    deals_controller修正

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,10 +1,11 @@
 class DealsController < ApplicationController
 
-  before_action :purchase_note, only: :create
+  # before_action :purchase_note, only: :create
+  before_action :authenticate_user!, only: :create
 
   def create
     @purchase = current_user.deals_of_subscriber.create(author_id: purchase_note.user_id, note_id: params[:note_id].to_i)
-    @notes = Note.order("created_at DESC")
+    redirect_to root_path
   end
 
   private
