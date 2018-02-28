789697dc07a64b21e90b28e5f5b025ee9848e64d
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-02-28 23:52:21 +0900

    モーダルウィンドウの見た目と動作を修正

--- app/assets/javascripts/modal_purchase.js
+++ app/assets/javascripts/modal_purchase.js
@@ -11,18 +11,29 @@ $(function() {
     });
   }
 
+  function buildHTML(data) {
+    var purchaseBtnHTML = `
+      <a class="modal-container__purchase-btn__post" data-method="post" href="/notes/${data}/deals">
+        <button class="modal-container__purchase-btn">購入</button>
+      </a>
+      `
+    return purchaseBtnHTML;
+  }
+
   locateCenter();
   $(window).resize(locateCenter);
 
   $('.author-purchase-btn').on('click', function() {
     var note_id = $(this).attr("value")
-    $(".modal-container__purchase-btn__post").attr("href", "/notes/" + note_id + "/deals");
-    $(".modal-container__purchase-btn__post").attr("data-method", "post");
+    var html = buildHTML(note_id)
+    $(".modal-container__modal-window__footer__purchase").append(html);
     $('.modal-container__overlay, .modal-container__modal-window').fadeIn();
+    debugger;
   });
 
   $('.modal-container__close-btn').on('click', function() {
     $('.modal-container__overlay, .modal-container__modal-window').fadeOut();
+    $(".modal-container__modal-window__footer__purchase").empty();
   });
 
 });
