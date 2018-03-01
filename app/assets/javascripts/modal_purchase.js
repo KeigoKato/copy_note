47d30d4d9455b8f169582e947f1af78dd5217f26
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-01 20:13:59 +0900

    モーダルウィンドウにタイトルと値段を表示

--- app/assets/javascripts/modal_purchase.js
+++ app/assets/javascripts/modal_purchase.js
@@ -3,37 +3,35 @@ $(function() {
   function locateCenter() {
     let w = $(window).width();
     let h = $(window).height();
-    let cw = $('.modal-container__modal-window').outerWidth();
-    let ch = $('.modal-container__modal-window').outerHeight();
-    $('.modal-container__modal-window').css({
-      'left': ((w - cw) / 2) + 'px',
-      'top': ((h - ch) / 2) + 'px'
+    let cw = $(".modal-container__modal-window").outerWidth();
+    let ch = $(".modal-container__modal-window").outerHeight();
+    $(".modal-container__modal-window").css({
+      "left": ((w - cw) / 2) + "px",
+      "top": ((h - ch) / 2) + "px"
     });
   }
 
-  function buildHTML(data) {
-    var purchaseBtnHTML = `
-      <a class="modal-container__purchase-btn__post" data-method="post" href="/notes/${data}/deals">
-        <button class="modal-container__purchase-btn">購入</button>
-      </a>
-      `
-    return purchaseBtnHTML;
+  function replaceHTML (title, price) {
+    $(".purchase-detail__title").text(title);
+    $(".purchase-detail__price").text(price);
   }
+
+  $(".author-purchase-btn").on("click", function() {
+    var note_id = $(this).attr("value");
+    var noteURL = "[href='/notes/" + note_id + "']"
+    var title = $(noteURL).text();
+    var price = $(this).find("p").text().replace(/../, "").replace(/$/, "円")
+    var postHref = "notes/" + note_id + "/deals";
+    replaceHTML(title, price);
+    $(".modal-container__purchase-btn").attr({"href":postHref, "data-method":"post"});
+    $(".modal-container__overlay, .modal-container__modal-window").fadeIn();
+  });
+
+  $(".modal-container__close-btn").on("click", function() {
+    $(".modal-container__overlay, .modal-container__modal-window").fadeOut();
+  });
 
   locateCenter();
   $(window).resize(locateCenter);
-
-  $('.author-purchase-btn').on('click', function() {
-    var note_id = $(this).attr("value")
-    var html = buildHTML(note_id)
-    $(".modal-container__modal-window__footer__purchase").append(html);
-    $('.modal-container__overlay, .modal-container__modal-window').fadeIn();
-    debugger;
-  });
-
-  $('.modal-container__close-btn').on('click', function() {
-    $('.modal-container__overlay, .modal-container__modal-window').fadeOut();
-    $(".modal-container__modal-window__footer__purchase").empty();
-  });
 
 });
