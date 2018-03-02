9b3e6371cc60b8937cecccd4ccffd9a7385861e4
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-02 20:08:32 +0900

    モーダルウィンドウにアラート表示機能を設ける

--- app/assets/javascripts/modal_purchase.js
+++ app/assets/javascripts/modal_purchase.js
@@ -22,13 +22,28 @@ $(function() {
     var title = $(noteURL).text();
     var price = $(this).find("p").text().replace(/../, "").replace(/$/, "円")
     var postHref = "notes/" + note_id + "/deals";
-    replaceHTML(title, price);
-    $(".modal-container__purchase-btn").attr({"href":postHref, "data-method":"post"});
-    $(".modal-container__overlay, .modal-container__modal-window").fadeIn();
+    if (price.match(/[0-9]+/)){
+      replaceHTML(title, price);
+      $(".modal-container__purchase-btn").attr({"href":postHref, "data-method":"post"});
+      $(".modal-container__overlay, .modal-container__modal-window").fadeIn();
+    } else {
+      alert("購入済みです");
+    }
   });
 
   $(".modal-container__close-btn").on("click", function() {
-    $(".modal-container__overlay, .modal-container__modal-window").fadeOut();
+    if (!confirm("本当に購入を中止しますか？")){
+      return false;
+    } else {
+      $(".modal-container__overlay, .modal-container__modal-window").fadeOut();
+    }
+  });
+
+  $(".modal-container__overlay").on("click", function(e){
+      var clickedElement = $(e.target);
+      if($(clickedElement).hasClass("modal-container__overlay")){
+          $(".modal-container__overlay").fadeOut();
+      }
   });
 
   locateCenter();
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_fit: [578,260]
+  process resize_to_fit: [538,260]
   storage :file
+
+  def extension_white_list
+    %W[jpg jpeg gif png]
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
