880b77b0a03fb6de791e1e0c7e7ceb3e88cdb143
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-04 16:08:02 +0900

    購入ボタンを押した後の動作を修正

--- app/assets/javascripts/modal_purchase.js
+++ app/assets/javascripts/modal_purchase.js
@@ -5,22 +5,28 @@ $(function() {
     $(".purchase-detail__price").text(price);
   }
 
-  $(".author-purchase-btn").on("click", function() {
-    var note_id = $(this).attr("value");
-    var noteURL = "[href='/notes/" + note_id + "']"
+  $(document).on("click", ".author-purchase-btn", function(e) {
+    var authorId = $(e.target).parent().parent().prev().find("a").attr("href").replace(/\/users\//, "")
+    var userId = $(".current_user_page").attr("href").replace(/\/users\//, "");
+    var noteId = $(this).attr("value");
+    var noteURL = "[href='/notes/" + noteId + "']"
     var title = $(noteURL).text();
     var price = $(this).find("p").text().replace(/../, "").replace(/$/, "円")
-    var postHref = "notes/" + note_id + "/deals";
-    if (price.match(/[0-9]+/)){
-      replaceHTML(title, price);
-      $(".modal-container__purchase-btn").attr({"href":postHref, "data-method":"post"});
-      $(".modal-container__overlay, .modal-container__modal-window").fadeIn();
+    var postHref = "notes/" + noteId + "/deals";
+    if (authorId !== userId) {
+      if (price.match(/[0-9]+/)){
+        replaceHTML(title, price);
+        $(".modal-container__purchase-btn").attr({"href":postHref, "data-method":"post"});
+        $(".modal-container__overlay, .modal-container__modal-window").fadeIn();
+      } else {
+        alert("購入済みです");
+      }
     } else {
-      alert("購入済みです");
+      alert("これはあなたが投稿したノートです。購入することはできません。")
     }
   });
 
-  $(".modal-container__close-btn").on("click", function() {
+  $(document).on("click", ".modal-container__close-btn", function() {
     if (!confirm("本当に購入を中止しますか？")){
       return false;
     } else {
@@ -28,7 +34,7 @@ $(function() {
     }
   });
 
-  $(".modal-container__overlay").on("click", function(e){
+  $(document).on("click", ".modal-container__overlay", function(e){
       var clickedElement = $(e.target);
       if($(clickedElement).hasClass("modal-container__overlay")){
           $(".modal-container__overlay").fadeOut();
