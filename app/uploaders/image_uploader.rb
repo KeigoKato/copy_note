7f4a463dcc88e4677adb47eadb0b7b7133ffec3e
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-04 19:25:52 +0900

    サムネイル画像の投稿が可能

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,15 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [578,266]
+
+  # version :title_image do
+  #   process resize_to_limit: [578, 238]
+  # end
+  process resize_to_limit: [578, 238]
+
   storage :file
 
   version :thumbnail do
-    process resize_to_fill: [36, 36]
+    process resize_to_fit: [36, 36]
   end
 
   def extension_white_list
