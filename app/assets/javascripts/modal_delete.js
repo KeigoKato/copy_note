dc28c3a1689cde72e8d9b118a2f74f32f6fd935e
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-09 01:10:41 +0900

    削除ボタンのビューの作成が完了

--- app/assets/javascripts/modal_delete.js
+++ app/assets/javascripts/modal_delete.js
@@ -1,8 +1,23 @@
 $(function (){
-  // $(document).on("click", ".fa-ellipsis-h", function(){
-  //   $(".modal-delete").fadeIn();
-  // });
-  $(document).on("click", ".fa-ellipsis-h", function(e){
-    debugger;
+  $(document).on("click", function(e){
+    var clickClass = $(e.target).attr("class")
+    if (clickClass == "fa fa-ellipsis-h") {
+      var target = $(e.target).parents(".articles").next();
+      if (!target.attr("style") || target.attr("style").match(/none/)) {
+        target.fadeIn();
+      }
+    }
+  });
+
+  $(document).on("click", ":not(.modal-delete[style='display: block;'])", function(e){
+    $(".modal-delete[style='display: block;']").fadeOut();
+  });
+
+  $(document).on("click", "#delete-note", function(){
+    if (!confirm("本当に削除しますか？")){
+      return false;
+    } else {
+      return true;
+    }
   });
 });
