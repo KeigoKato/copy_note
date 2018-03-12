23af571298033f064226cdef9c38fd8dfa8138a2
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-12 20:33:33 +0900

    [add] reviewsコントローラのcreateアクションを記述 入力した情報をparamsで受け取ってDBに保存する。保存後は記事の詳細ページにリダイレクトする。まずhtmlによる通信を実装して、それができたらjson形式に変更する。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def create
+    review = Review.new(create_review)
+    review.save
+    redirect_to "/notes/params[:note_id]"
+  end
+
+  private
+  def create_review
+    params.require(:review).params(:review).merge(user_id: current_user.id, note_id: params[:note_id])
 end
