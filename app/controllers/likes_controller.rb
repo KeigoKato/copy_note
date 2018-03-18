b53175a2099c1aede3a065e2d8363cec810ba8fa
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-18 16:45:11 +0900

    [update] likesコントローラを修正
    「いいね」ボタンを押した後の記事を表すインスタンス変数(@notes)を投稿と逆の順になるように定義する。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, note_id: params[:note_id])
     @likes = Like.where(note_id: params[:note_id])
-    @notes = Note.all
+    @notes = Note.order("created_at DESC").includes(:user)
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, note_id: params[:note_id])
     like.destroy
     @likes = Like.where(note_id: params[:note_id])
-    @notes= Note.all
+    @notes = Note.order("created_at DESC").includes(:user)
   end
 end
