84a02e2e2cfbef628cd3203571cfa2da04110ea4
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-18 16:33:38 +0900

    [add] likesテーブルを作成
    カラムはuser_idとnote_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312103731) do
+ActiveRecord::Schema.define(version: 20180318072955) do
 
   create_table "deals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "author_id"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180312103731) do
     t.integer  "note_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
