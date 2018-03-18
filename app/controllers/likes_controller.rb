375c55bd683b1a96a6629e1f607a344be6807eff
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-18 16:42:05 +0900

    [add] likesコントローラのアクションを定義

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, note_id: params[:note_id])
+    @likes = Like.where(note_id: params[:note_id])
+    @notes = Note.all
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, note_id: params[:note_id])
+    like.destroy
+    @likes = Like.where(note_id: params[:note_id])
+    @notes= Note.all
+  end
+end
