e43e746a64bbb506e77af4050eca7c2e637a9a09
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-18 20:59:06 +0900

    [add] 「いいね」機能の基本を実装
    ボタンクリックで「いいね」のON/OFFを切り替えられるようになった。「いいね」のカウント数とビューはまだ完成していない。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,20 @@
 class LikesController < ApplicationController
-  before_action :set_note, only: [:create, :destroy]
-
   def create
     @like = current_user.likes.create(note_id: params[:note_id])
-    @notes = note.all
+    @notes = Note.order("created_at DESC").includes(:user)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
     like = current_user.likes.find_by(note_id: params[:note_id])
     like.destroy
-    @notes = note.all
+    @notes = Note.order("created_at DESC").includes(:user)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
-
-  private
-
-  def set_note
-    @note = note.find(params[:note_id])
-  end
-
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :note, counter_cache: :likes_count
+  # belongs_to :note, counter_cache: :likes_count
+  belongs_to :note
   belongs_to :user
 end
