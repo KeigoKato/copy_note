0f100a6a061083266f83000da30643eaee08110e
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-21 12:28:03 +0900

    [update] 編集画面で既存のタグが出力される

--- app/assets/javascripts/note_tag.js
+++ app/assets/javascripts/note_tag.js
@@ -1,6 +1,11 @@
 $(function(){
   $(document).on('turbolinks:load', function() {
     $('#note-tags').tagit({ placeholderText: "タグをつけよう", fieldName: "note[tag_list]", singleField: true });
+    if(gon.note_tags){
+      for (var tag in gon.note_tags){
+      $("#note-tags").tagit("createTag", tag);
+      }
+    }
   });
 });
 
